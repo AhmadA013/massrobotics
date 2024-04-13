2a2f2c7cb9981f16a520d43172fb2ccae9bbd4b7
nmathira <mathirajanniranjan@gmail.com>
2024-04-13 00:26:34 -0400

    feat(controller): Implemented WAP and basic TCP Server

--- robot-controller/src/main.cpp
+++ robot-controller/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "robot-wifi";
+const char *password = "123456789";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const uint SERVER_PORT = 22;
+WiFiServer server(SERVER_PORT);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Hello world");
+
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    WiFiClient client = server.available(); 
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (client) {
+        Serial.println("New Client."); 
+        Serial.println(client.localIP());
+        while (client.connected()) {            // loop while the client's connected
+            if (client.available()) { 
+                String msg = client.readString();
+                Serial.print(msg);
+            }
+        }
+    }
 }
